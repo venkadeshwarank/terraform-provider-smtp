c63cfe39d8d488f2ff2cfd0b5fcce2d2e28e320a
Venkad <venkadeshwaran.karunakaran@intel.com>
2023-06-02 17:31:38 +0530

    Fixing example

--- examples/resources/smtp_send_mail/resource.tf
+++ examples/resources/smtp_send_mail/resource.tf
@@ -1,5 +1,5 @@
 resource "smtp_send_mail" "this" {
-  to      = "to@example.com"
+  to      = ["to@example.com"]
   from    = "from@example.com"
   subject = "First Terraform plugin"
   body    = "My first mail goes good."
